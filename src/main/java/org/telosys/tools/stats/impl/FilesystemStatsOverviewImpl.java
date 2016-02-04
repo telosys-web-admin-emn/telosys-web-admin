3d21670e83c6c0b25a0ef122c18d111ee9fd7070
maeln <contact@maeln.com>
2016-02-04 14:52:28 +0100

    Implemented userCount.

--- src/main/java/org/telosys/tools/stats/impl/FilesystemStatsOverviewImpl.java
+++ src/main/java/org/telosys/tools/stats/impl/FilesystemStatsOverviewImpl.java
@@ -1,13 +1,41 @@
 package org.telosys.tools.stats.impl;
 
+import org.telosys.tools.helper.CSVReader;
+import org.telosys.tools.stats.Configuration;
 import org.telosys.tools.stats.FilesystemStatsOverview;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FilesystemStatsOverviewImpl implements FilesystemStatsOverview  {
+	private File root;
+
+	public FilesystemStatsOverviewImpl(File root)
+	{
+		this.root = root;
+	}
 
 	@Override
 	public int getUsersCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		FileReader user_file = new FileReader(this.root.getAbsolutePath()+'/fs/users.csv');
+		BufferedReader buffer = new BufferedReader(user_file);
+		StringBuffer res = new StringBuffer();
+
+		try
+		{
+			for(String line = buffer.readLine(); line != null; line = buffer.readLine())
+			{
+				res.append(line + "\n");
+			}
+			CSVReader csv = new CSVReader(res.toString());
+			return csv.numberOfLines();
+		}
+		catch (IOException e)
+		{
+			return -1;
+		}
 	}
 
 	@Override
