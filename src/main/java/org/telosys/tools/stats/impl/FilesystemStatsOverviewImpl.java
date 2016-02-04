afb54d97b9e906c87cff828d5ddcf70cb6c8d1c8
maeln <contact@maeln.com>
2016-02-04 16:00:05 +0100

    Added getProjectCount.

--- src/main/java/org/telosys/tools/stats/impl/FilesystemStatsOverviewImpl.java
+++ src/main/java/org/telosys/tools/stats/impl/FilesystemStatsOverviewImpl.java
@@ -5,30 +5,32 @@ import org.telosys.tools.helper.CSVReader;
 import org.telosys.tools.stats.FilesystemStatsOverview;
 import org.telosys.tools.stats.services.FilesystemUtil;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FilesystemStatsOverviewImpl implements FilesystemStatsOverview  {
 	private File root;
+	private CSVReader users;
 
-	public FilesystemStatsOverviewImpl(File root)
-	{
+
+	public FilesystemStatsOverviewImpl(File root) {
 		this.root = root;
+	}
+
+	private void readUsersFile() throws IOException {
+		FileReader user_file = new FileReader(this.root.getAbsolutePath()+"/users.csv");
+		BufferedReader buffer = new BufferedReader(user_file);
+		StringBuffer res = new StringBuffer();
+		for(String line = buffer.readLine(); line != null; line = buffer.readLine()) {
+			res.append(line + "\n");
+		}
+		this.users = new CSVReader(res.toString());
 	}
 
 	@Override
 	public int getUsersCount() {
 		try {
-			FileReader user_file = new FileReader(this.root.getAbsolutePath()+"/fs/users.csv");
-			BufferedReader buffer = new BufferedReader(user_file);
-			StringBuffer res = new StringBuffer();
-			for(String line = buffer.readLine(); line != null; line = buffer.readLine()) {
-				res.append(line + "\n");
-			}
-			CSVReader csv = new CSVReader(res.toString());
-			return csv.numberOfLines();
+			this.readUsersFile();
+			return this.users.numberOfLines();
 		}
 		catch (IOException e) {
 			return -1;
@@ -37,8 +39,18 @@ public class FilesystemStatsOverviewImpl implements FilesystemStatsOverview  {
 
 	@Override
 	public int getProjectsCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		int somme = 0;
+		try {
+			this.readUsersFile();
+			for(int i=0; i<this.users.numberOfLines(); ++i) {
+				UsersStatsImpl usersStats = new UsersStatsImpl(this.users.getColumn(i,0));
+				somme += usersStats.getProjectsCount();
+			}
+			return somme;
+		} catch (IOException e) {
+			return -1;
+		}
+
 	}
 
 	@Override
