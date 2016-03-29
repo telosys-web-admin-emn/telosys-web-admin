b2dd9c0dfa055f8a02a34729097500e71b1ee28e
maeln <contact@maeln.com>
2016-03-29 16:49:39 +0200

    Implementation de BundleStatsImpl.

--- src/main/java/org/telosys/tools/stats/impl/BundleStatsImpl.java
+++ src/main/java/org/telosys/tools/stats/impl/BundleStatsImpl.java
@@ -1,27 +1,51 @@
 package org.telosys.tools.stats.impl;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 
 import org.telosys.tools.stats.BundleStats;
 
 public class BundleStatsImpl implements BundleStats {
 
+	private File root;
+	private String userName;
+	private String bundleName;
+	private String projectName;
+
+	public BundleStatsImpl(File root, String userName, String bundleName, String projectName) {
+		this.root = root;
+		this.userName = userName;
+		this.bundleName = bundleName;
+		this.projectName = projectName;
+	}
+
 	@Override
 	public String getProjectName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.projectName;
 	}
 
 	@Override
 	public String getBundleName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.bundleName;
 	}
 
 	@Override
 	public Date getInstallationDate() {
-		// TODO Auto-generated method stub
-		return null;
+		Path bundle_path = this.root.toPath().resolve(this.userName).resolve(this.projectName);
+		bundle_path.resolve("TelosysTools/templates").resolve(this.bundleName);
+		BasicFileAttributes attr = null;
+		try
+		{
+			attr = Files.readAttributes(bundle_path, BasicFileAttributes.class);
+			return new Date(attr.creationTime().toMillis());
+		} catch(IOException e)
+		{
+			return null;
+		}
 	}
 
 	@Override
