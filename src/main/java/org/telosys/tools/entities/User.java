bbaac24188944f571b4890869cf3c905d55116a7
Alexandre LEBRUN <alex.3385@hotmail.fr>
2016-03-29 14:23:31 +0200

    Refactor CsvReader to use JacksonCSV

---
+++ src/main/java/org/telosys/tools/entities/User.java
@@ -0,0 +1,32 @@
+package org.telosys.tools.entities;
+
+/**
+ * Created by alexa on 29/03/2016.
+ */
+public class User {
+
+    private String username;
+
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/org/telosys/tools/helper/UsersCsvParser.java
@@ -0,0 +1,44 @@
+package org.telosys.tools.helper;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.telosys.tools.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Helper classe to parse CSV file.
+ *
+ * @author Maël Naccache
+ * @version 1.0
+ */
+public class UsersCsvParser {
+
+	private File file;
+
+	public UsersCsvParser(File file) {
+		this.file = file;
+	}
+
+
+	private List<User> parse() throws IOException {
+		List<User> users = new ArrayList<>();
+		CsvMapper mapper = new CsvMapper();
+		mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
+		Iterator<String[]> it = mapper.readerFor(String[].class).readValues(file);
+		while(it.hasNext()) {
+			String[] row = it.next();
+			users.add(new User(row[0], row[2]));
+		}
+		return users;
+	}
+
+}
