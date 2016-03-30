2489d2fc190d38dbb27fe81449f1f81ad200f886
Alexandre LEBRUN <alex.3385@hotmail.fr>
2016-03-30 09:19:39 +0200

    Add WelcomeAction

---
+++ src/main/java/org/telosys/admin/actions/WelcomeAction.java
@@ -0,0 +1,17 @@
+package org.telosys.admin.actions;
+
+import org.nanoj.web.tinymvc.GenericAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by alexa on 30/03/2016.
+ */
+public class WelcomeAction extends GenericAction {
+
+    @Override
+    public String process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        return "index";
+    }
+}
