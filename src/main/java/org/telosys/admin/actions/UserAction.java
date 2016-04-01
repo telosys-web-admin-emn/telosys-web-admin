a08712e3f10262febcf0239d64d754f9331b744a
maeln <contact@maeln.com>
2016-04-01 14:32:59 +0200

    Implementation de UserAction.

--- src/main/java/org/telosys/admin/actions/UserAction.java
+++ src/main/java/org/telosys/admin/actions/UserAction.java
@@ -1,17 +1,25 @@
 package org.telosys.admin.actions;
 
 import org.nanoj.web.tinymvc.GenericAction;
+import org.telosys.tools.stats.Configuration;
+import org.telosys.tools.api.TelosysUsers;
+import org.telosys.tools.users.User;
+import org.telosys.tools.users.UsersFileName;
+import org.telosys.tools.users.UsersManager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.ParseException;
 
-/**
- * Created by alexa on 30/03/2016.
- */
 public class UserAction extends GenericAction {
 
     @Override
     public String process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+	    UsersFileName.setSpecificFileName(Configuration.getTelosysSaasLocation()+"/fs/users.csv");
+	    UsersManager users = UsersManager.getInstance();
+	    User mu = users.getUserByLogin("maeln");
+	    httpServletRequest.setAttribute("user", mu);
+
         return "user";
     }
 }
