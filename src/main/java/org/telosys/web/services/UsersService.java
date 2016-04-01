1e90a532542e7c14fede10851cb36a21b3e6f4c9
naokha <xavier.huberdeau@gmail.com>
2016-04-01 14:31:08 +0200

    Users view

--- src/main/java/org/telosys/web/services/UsersService.java
+++ src/main/java/org/telosys/web/services/UsersService.java
@@ -3,10 +3,12 @@ package org.telosys.web.services;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.telosys.tools.entities.User;
 import org.telosys.tools.helper.UsersCsvParser;
+import org.telosys.tools.stats.impl.UsersStatsImpl;
 
 public class UsersService {
 	private File usersCsvFile;
@@ -16,10 +18,16 @@ public class UsersService {
 		this.usersCsvFile = new File(usersCsvFilePath);
 	}
 				
-	public List<User> getUsers() throws IOException, ParseException
+	public List<UsersStatsImpl> getUsers() throws IOException, ParseException
 	{
 		UsersCsvParser uCP = new UsersCsvParser(this.usersCsvFile);
+		List<UsersStatsImpl> users = new ArrayList<UsersStatsImpl>();
+		String s = File.separator;
+    	File root = new File("C:"+s+"Users"+s+"Xavier"+s+"git"+s+"telosys-saas"+s+"fs"+s);
+		for(User u:uCP.parse()) {
+			users.add(new UsersStatsImpl(u, root));
+		}
 		
-		return uCP.parse();
+		return users;
 	}
 }
