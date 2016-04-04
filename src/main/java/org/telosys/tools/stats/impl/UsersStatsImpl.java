0aa90e53db951b5ec1f2ca25bb59cfa0a0865dbd
naokha <xavier.huberdeau@gmail.com>
2016-04-04 16:05:27 +0200

    Check that the user directory exists

--- src/main/java/org/telosys/tools/stats/impl/UsersStatsImpl.java
+++ src/main/java/org/telosys/tools/stats/impl/UsersStatsImpl.java
@@ -53,25 +53,33 @@ public class UsersStatsImpl implements UserStats {
 
 	@Override
 	public int getProjectsCount() {
-		return this.userDir.listFiles(File::isDirectory).length;
+		if (this.userDir.exists()) {
+			return this.userDir.listFiles(File::isDirectory).length;
+		}
+		return 0;
 	}
 
 	@Override
 	public List<String> getProjectsNames() {
-		return asList(this.userDir.listFiles())
-			.stream()
-			.map(File::getName)
-			.collect(toList());
+		if (this.userDir.exists()) {
+			return asList(this.userDir.listFiles())
+				.stream()
+				.map(File::getName)
+				.collect(toList());
+		}
+		return new ArrayList<String>();
 	}
 
 	@Override
 	public int getModelsCount() {
 		int count = 0;
-		for(File project:this.userDir.listFiles()){
-			File telosysToolsDir = new File(project.getAbsolutePath() + "/TelosysTools");
-			if (telosysToolsDir.isDirectory()) {
-				count += telosysToolsDir.listFiles(f -> f.getName().toLowerCase().endsWith(".model")).length;
-			}			
+		if (this.userDir.exists()) {
+			for(File project:this.userDir.listFiles()){
+				File telosysToolsDir = new File(project.getAbsolutePath() + "/TelosysTools");
+				if (telosysToolsDir.isDirectory()) {
+					count += telosysToolsDir.listFiles(f -> f.getName().toLowerCase().endsWith(".model")).length;
+				}			
+			}
 		}
 		return count;
 	}
@@ -79,30 +87,38 @@ public class UsersStatsImpl implements UserStats {
 	@Override
 	public int getBundlesCount() {
 		int count = 0;
-		for(File project:this.userDir.listFiles()){
-			File templatesDir = new File(project.getAbsolutePath() + "/TelosysTools/templates");
-			count += templatesDir.listFiles(File::isDirectory).length;
-			
+		if (this.userDir.exists()) {
+			for(File project:this.userDir.listFiles()){
+				File templatesDir = new File(project.getAbsolutePath() + "/TelosysTools/templates");
+				count += templatesDir.listFiles(File::isDirectory).length;
+				
+			}
 		}
 		return count;
 	}
 
 	@Override
 	public List<String> getBundlesNames() {
-		List<String> bundlesNames = new ArrayList<>();
-		for(File project:this.userDir.listFiles()){
-			File templatesDir = new File(project.getAbsolutePath() + "/TelosysTools/templates");
-			bundlesNames.addAll(asList(templatesDir.listFiles(File::isDirectory))
-					.stream()
-					.map(File::getName)
-					.collect(toList()));
+		List<String> bundlesNames = new ArrayList<String>();
+		if (this.userDir.exists()) {
+			for(File project:this.userDir.listFiles()){
+				File templatesDir = new File(project.getAbsolutePath() + "/TelosysTools/templates");
+				bundlesNames.addAll(asList(templatesDir.listFiles(File::isDirectory))
+						.stream()
+						.map(File::getName)
+						.collect(toList()));
+			}
 		}
 		return bundlesNames;
 	}
 
 	@Override
 	public long getDiskUsage() {
-		return FileUtils.sizeOfDirectory(this.userDir);
+		if (this.userDir.exists()) {
+			return FileUtils.sizeOfDirectory(this.userDir);
+		}
+		return 0;
+		
 	}
 
 	@Override
