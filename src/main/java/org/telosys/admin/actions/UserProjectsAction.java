0fb41d84707b137b27924c6a1dece78a9a5517a4
naokha <xavier.huberdeau@gmail.com>
2016-04-04 15:36:03 +0200

    Comparator

---
+++ src/main/java/org/telosys/admin/actions/UserProjectsAction.java
@@ -0,0 +1,25 @@
+package org.telosys.admin.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.nanoj.web.tinymvc.GenericAction;
+import org.telosys.tools.stats.Configuration;
+import org.telosys.tools.users.User;
+import org.telosys.tools.users.UsersFileName;
+import org.telosys.tools.users.UsersManager;
+
+public class UserProjectsAction extends GenericAction {
+
+    @Override
+    public String process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+    	String userName = httpServletRequest.getParameter("username");
+    	if (userName != null) {
+    		UsersFileName.setSpecificFileName(Configuration.getTelosysSaasLocation()+"/fs/users.csv");
+    	    UsersManager users = UsersManager.getInstance();
+    	    User mu = users.getUserByLogin(userName);
+    	    httpServletRequest.setAttribute("user", mu);
+    	}
+        return "user";
+    }
+}
--- src/main/java/org/telosys/admin/actions/UsersAction.java
+++ src/main/java/org/telosys/admin/actions/UsersAction.java
@@ -3,8 +3,10 @@ package org.telosys.admin.actions;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Comparator;
 
 import org.nanoj.web.tinymvc.GenericAction;
+import org.telosys.tools.stats.impl.UsersStatsImpl;
 import org.telosys.web.services.UsersService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +20,12 @@ public class UsersAction extends GenericAction {
 		String usersFilePath = "C:"+s+"Users"+s+"Xavier"+s+"git"+s+"telosys-saas"+s+"fs"+s+"users.csv"+s;
     	UsersService usersService = new UsersService(usersFilePath);
     	try {
+    		Comparator comparator = new Comparator<UsersStatsImpl>(){
+    			public int compare(UsersStatsImpl u1, UsersStatsImpl u2) {
+    				return u1.getLogin().compareTo(u2.getLogin());
+    			}
+    		};
+    		httpServletRequest.setAttribute("comparator", comparator);
 			httpServletRequest.setAttribute("users", usersService.getUsers());
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
