722861890e7b5673db4c21450656fca2cd49857f
Alexandre LEBRUN <alex.3385@hotmail.fr>
2016-04-04 17:49:16 +0200

    Move user project stats to user stats

--- src/main/java/org/telosys/admin/actions/UserAction.java
+++ src/main/java/org/telosys/admin/actions/UserAction.java
@@ -1,43 +1,57 @@
 package org.telosys.admin.actions;
 
 import org.nanoj.web.tinymvc.GenericAction;
-import org.telosys.tools.stats.Configuration;
+import org.telosys.tools.stats.*;
 import org.telosys.tools.api.TelosysUsers;
-import org.telosys.tools.stats.UserStats;
+import org.telosys.tools.stats.exception.ProjectNotFoundException;
 import org.telosys.tools.stats.impl.UsersStatsImpl;
 import org.telosys.tools.users.User;
 import org.telosys.tools.users.UsersFileName;
 import org.telosys.tools.users.UsersManager;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserAction extends GenericAction {
 
-    @Override
-    public String process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-	    try
-	    {
-		    String userName = httpServletRequest.getParameter("username");
-		    if (userName != null) {
-		    	UsersFileName.setSpecificFileName(Configuration.getTelosysSaasLocation()+"/fs/users.csv");
-			    UsersManager users = UsersManager.getInstance();
-			    User myUser = users.getUserByLogin(userName);
-			    UserStats myUserstat = new UsersStatsImpl(myUser, new File(Configuration.getTelosysSaasLocation()+"/fs"));
-			    httpServletRequest.setAttribute("user", myUser);
-			    httpServletRequest.setAttribute("user_stat", myUserstat);
-		    }
-	    } catch(IOException e)
-	    {
-		    e.printStackTrace();
-	    } catch(ParseException e)
-	    {
-		    e.printStackTrace();
-	    }
+	StatsProvider provider = StatsProviderFactory.getStatsProvider();
 
-        return "user";
-    }
+	@Override
+	public String process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+		try
+		{
+
+
+			String userName = httpServletRequest.getParameter("username");
+			if (userName != null) {
+				// Step 1 : retrieve user project stats
+				UserStats usersStats = provider.getUserStats(userName);
+				List<ProjectStats> projectStats = new ArrayList<>();
+				for (String project : usersStats.getProjectsNames()) {
+					projectStats.add(provider.getProjectStats(userName, project));
+				}
+				httpServletRequest.setAttribute("projectStats", projectStats);
+
+				// Step 2 : retrieve user stats
+				UsersFileName.setSpecificFileName(Configuration.getTelosysSaasLocation()+"/fs/users.csv");
+				UsersManager users = UsersManager.getInstance();
+				User myUser = users.getUserByLogin(userName);
+				UserStats myUserstat = new UsersStatsImpl(myUser, new File(Configuration.getTelosysSaasLocation()+"/fs"));
+				httpServletRequest.setAttribute("user", myUser);
+				httpServletRequest.setAttribute("userStats", myUserstat);
+
+			}
+		} catch(IOException | ParseException | ProjectNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+		return "user";
+	}
 }
