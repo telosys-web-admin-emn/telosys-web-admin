61a24b0b8c9bff7925c8c53fe59d0ded5289dcb9
naokha <xavier.huberdeau@gmail.com>
2016-04-04 14:24:30 +0200

    Handling username parameter in UserAction

--- src/main/java/org/telosys/admin/actions/UserAction.java
+++ src/main/java/org/telosys/admin/actions/UserAction.java
@@ -1,25 +1,25 @@
 package org.telosys.admin.actions;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.nanoj.web.tinymvc.GenericAction;
 import org.telosys.tools.stats.Configuration;
-import org.telosys.tools.api.TelosysUsers;
 import org.telosys.tools.users.User;
 import org.telosys.tools.users.UsersFileName;
 import org.telosys.tools.users.UsersManager;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.text.ParseException;
 
 public class UserAction extends GenericAction {
 
     @Override
     public String process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-	    UsersFileName.setSpecificFileName(Configuration.getTelosysSaasLocation()+"/fs/users.csv");
-	    UsersManager users = UsersManager.getInstance();
-	    User mu = users.getUserByLogin("maeln");
-	    httpServletRequest.setAttribute("user", mu);
-
+    	String userName = httpServletRequest.getParameter("username");
+    	if (userName != null) {
+    		UsersFileName.setSpecificFileName(Configuration.getTelosysSaasLocation()+"/fs/users.csv");
+    	    UsersManager users = UsersManager.getInstance();
+    	    User mu = users.getUserByLogin(userName);
+    	    httpServletRequest.setAttribute("user", mu);
+    	}
         return "user";
     }
 }
