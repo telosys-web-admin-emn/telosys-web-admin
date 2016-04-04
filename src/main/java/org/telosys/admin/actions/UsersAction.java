2535fdecd831f6ef947ecd636ef3e98f927c8be9
naokha <xavier.huberdeau@gmail.com>
2016-04-04 16:56:19 +0200

    Added sort on users view

--- src/main/java/org/telosys/admin/actions/UsersAction.java
+++ src/main/java/org/telosys/admin/actions/UsersAction.java
@@ -20,17 +20,60 @@ public class UsersAction extends GenericAction {
 		String usersFilePath = Configuration.getTelosysSaasLocation() + "/fs/user.csv";
     	UsersService usersService = new UsersService(usersFilePath);
     	try {
-    		Comparator comparator = new Comparator<UsersStatsImpl>(){
-    			public int compare(UsersStatsImpl u1, UsersStatsImpl u2) {
-    				return u1.getLogin().compareTo(u2.getLogin());
-    			}
-    		};
-    		httpServletRequest.setAttribute("comparator", comparator);
+    		
+    		httpServletRequest.setAttribute("comparator", this.getUsersComparator(httpServletRequest));
 			httpServletRequest.setAttribute("users", usersService.getUsers());
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
     	return "users";
+    }
+    
+    /**
+     * Get the users comparator according to the filter parameter. Only ascending order is handled at the moment
+     * @param httpServletRequest
+     * @return Comparator
+     */
+    protected Comparator<UsersStatsImpl> getUsersComparator(HttpServletRequest httpServletRequest)
+    {
+    	Comparator<UsersStatsImpl> comparator = new Comparator<UsersStatsImpl>(){
+			public int compare(UsersStatsImpl u1, UsersStatsImpl u2) {
+				String filter = httpServletRequest.getParameter("filter");
+		    	if(filter == null) {
+		    		filter = "login";
+		    	}
+		    	String order = httpServletRequest.getParameter("order");
+		    	// the result of the comparison
+		    	int compared = 0;
+				switch(filter){
+					case "creationDate":
+						compared = u1.getCreationDate().compareTo(u2.getCreationDate());
+						break;
+					case "mail":
+						compared =  u1.getMail().compareTo(u2.getMail());
+					case "diskUsage":
+						compared = Double.compare(u1.getDiskUsage(), u2.getDiskUsage());
+						break;
+					case "projectsCount":
+						compared = Integer.compare(u1.getProjectsCount(), u2.getProjectsCount());
+						break;
+					case "modelsCount":
+						compared = Integer.compare(u1.getModelsCount(), u2.getModelsCount());
+						break;
+					case "generationsCount":
+						compared = Integer.compare(u1.getGenerationsCount(), u2.getGenerationsCount());
+						break;
+					default:
+						compared = u1.getLogin().compareTo(u2.getLogin());
+				}
+				// check if we have to reverse the order
+				if(order != null && order.equals("DESC")) {
+					compared = -compared;
+				}
+				return compared;
+			}
+		};
+		return comparator;
     }
 
 }
